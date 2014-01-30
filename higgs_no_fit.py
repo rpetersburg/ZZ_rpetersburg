c70885a71417a4ff0736d59e56f25a4d005d11bd
rpetersburg <rpetersburg@gmail.com>
2014-01-30 15:17:31 +0100

    Added general higgs plotting script

---
+++ higgs_no_fit.py
@@ -0,0 +1,32 @@
+from ROOT import *
+gROOT.LoadMacro('atlasstyle/AtlasStyle.C')
+import PlotHiggs
+SetAtlasStyle()
+
+
+higgs = PlotHiggs.PlotHiggs()
+
+dataFiles = ['data11','data12']
+dataHistogram = TH1F( 'dataHistogram', 'Data Histogram', higgs.nBins, higgs.lowerLimit, higgs.upperLimit )
+higgs.setHistogram(dataHistogram, dataFiles)
+
+mcJetBkgFiles = ['out_redBkg_Comb','out_redBkg_Comb']
+mcJetBkgHistogram = TH1F( 'mcJetBkgHistogram', 'MC Jet Background Histogram', higgs.nBins, higgs.lowerLimit, higgs.upperLimit)
+higgs.setHistogramJets(mcJetBkgHistogram, mcJetBkgFiles)
+higgs.formatHistogram(mcJetBkgHistogram, kViolet)
+
+mcZZBkgFiles = ['mc11_ZZComb','mc12_ZZComb']
+mcZZBkgHistogram = TH1F( 'mcZZBkgHistogram', 'MC ZZ Background Histogram', higgs.nBins, higgs.lowerLimit, higgs.upperLimit)
+higgs.setHistogram(mcZZBkgHistogram, mcZZBkgFiles)
+higgs.formatHistogram(mcZZBkgHistogram, kRed)
+
+mcSignalFiles = ['mc11_signalComb','mc12_signalComb']
+mcSignalHistogram = TH1F( 'mcSignalHistogram', 'MC Signal Histogram', higgs.nBins, higgs.lowerLimit, higgs.upperLimit )
+higgs.setHistogram(mcSignalHistogram, mcSignalFiles)
+higgs.formatHistogram(mcSignalHistogram, kCyan)
+
+histogramList = [dataHistogram, mcJetBkgHistogram, mcZZBkgHistogram, mcSignalHistogram]
+histogramNames = ['Experimental Data', 'MC Background Z+jets, t#bar{t}', 'MC Background ZZ^{(*)}', 'MC Signal (m_{H} = 125 GeV)']
+histogramOptions = ['pe','f','f','f']
+
+higgs.drawCombinedHistogram(histogramList,histogramNames, histogramOptions)
